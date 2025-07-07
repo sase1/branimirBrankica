94cb35e72cbeef588d79b6afb0de66905f752f54
sase1 <saso_mitkovski@live.com>
2025-07-07 10:40:56 +0200

    contact page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+// import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import {HashRouter  as Router, Routes, Route} from "react-router-dom";
 import Home from "./pages/Home";
 import AboutUs from "./pages/AboutUs";
 import Contact from "./pages/Contact";
