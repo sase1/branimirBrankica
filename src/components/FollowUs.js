3d2280f23a6a24fdd7e56bd797b52ed269256ba1
sase1 <saso_mitkovski@live.com>
2025-07-21 09:41:06 +0200

    UI upadtes

--- src/components/FollowUs.js
+++ src/components/FollowUs.js
@@ -9,32 +9,32 @@ export default function FollowUs() {
             </div>
             <div className="col-lg-2 col-md-4 col-6 item item1">
                 <Link to="https://www.instagram.com/branimir_i_brankica/?hl=en" target="_blank" className="overlay text-decoration-none text-white">
-                    <img className="white-insta-logo" src={`${process.env.PUBLIC_URL}/images/see-insta-icon.svg`} alt="instagram-logo"/>Види
+                    <img className="white-insta-logo" src={`${process.env.PUBLIC_URL}/images/see-insta-icon.svg`} alt="instagram-logo"/> Види
                     на Инстаграм</Link>
             </div>
             <div className="col-lg-2 col-md-4 col-6 item item2">
                 <Link to="https://www.instagram.com/branimir_i_brankica/?hl=en" target="_blank" className="overlay text-decoration-none text-white">
-                    <img className="white-insta-logo" src={`${process.env.PUBLIC_URL}/images/see-insta-icon.svg`} alt="instagram-logo"/>Види
+                    <img className="white-insta-logo" src={`${process.env.PUBLIC_URL}/images/see-insta-icon.svg`} alt="instagram-logo"/> Види
                     на Инстаграм</Link>
             </div>
             <div className="col-lg-2 col-md-4 col-6 item item3">
             <Link to="https://www.instagram.com/branimir_i_brankica/?hl=en" target="_blank" className="overlay text-decoration-none text-white">
-                <img className="white-insta-logo" src={`${process.env.PUBLIC_URL}/images/see-insta-icon.svg`} alt="instagram-logo"/>Види
+                <img className="white-insta-logo" src={`${process.env.PUBLIC_URL}/images/see-insta-icon.svg`} alt="instagram-logo"/> Види
                 на Инстаграм</Link>
             </div>
             <div className="col-lg-2 col-md-4 col-6 item item4">
             <Link to="https://www.instagram.com/branimir_i_brankica/?hl=en" target="_blank" className="overlay text-decoration-none text-white">
-                <img className="white-insta-logo" src={`${process.env.PUBLIC_URL}/images/see-insta-icon.svg`} alt="instagram-logo"/>Види
+                <img className="white-insta-logo" src={`${process.env.PUBLIC_URL}/images/see-insta-icon.svg`} alt="instagram-logo"/> Види
                 на Инстаграм</Link>
             </div>
             <div className="col-lg-2 col-md-4 col-6 item item5">
             <Link to="https://www.instagram.com/branimir_i_brankica/?hl=en" target="_blank" className="overlay text-decoration-none text-white">
-                <img className="white-insta-logo" src={`${process.env.PUBLIC_URL}/images/see-insta-icon.svg`} alt="instagram-logo"/>Види
+                <img className="white-insta-logo" src={`${process.env.PUBLIC_URL}/images/see-insta-icon.svg`} alt="instagram-logo"/> Види
                 на Инстаграм</Link>
             </div>
             <div className="col-lg-2 col-md-4 col-6 item item6">
             <Link to="https://www.instagram.com/branimir_i_brankica/?hl=en" target="_blank" className="overlay text-decoration-none text-white">
-                <img className="white-insta-logo" src={`${process.env.PUBLIC_URL}/images/see-insta-icon.svg`} alt="instagram-logo"/>Види
+                <img className="white-insta-logo" src={`${process.env.PUBLIC_URL}/images/see-insta-icon.svg`} alt="instagram-logo"/> Види
                 на Инстаграм</Link>
             </div>
             <div className="insta d-flex align-items-center justify-content-center">
